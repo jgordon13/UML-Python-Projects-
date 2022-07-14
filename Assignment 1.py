c27101046602e3af715c4259c054b73e8e949de2
Julia Gordon <98852088+jgordon13@users.noreply.github.com>
2022-07-14 11:29:00 -0400

    uploaded programming assignment #1
    this is a very simple code I did as an introduction to python

---
+++ Assignment 1.py
@@ -0,0 +1,10 @@
+# Julia Gordon
+# 6/9/21
+# Programming Assignment 1 
+
+# Write a program that prints an introduction message with your name and does the following mathematical operation. (4x+3)/0.2
+
+x=9
+result=(4*x+3)/0.2
+name="Julia"
+print(f"Hello, welcome to my python program! My name is {name}. The value of x is {x}. The result of (4x+3)/0.2 is {result}.")
